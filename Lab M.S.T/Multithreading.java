8c98c8bbd6cb0d02289083af4fe7f11358707ce1
KalkiNara <163158995+KalkiNara@users.noreply.github.com>
2025-10-01 16:09:18 +0530

    Add files via upload

---
+++ Lab M.S.T/Multithreading.java
@@ -0,0 +1,26 @@
+class NumberThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+            try { Thread.sleep(500); } catch (InterruptedException e) {}
+        }
+    }
+}
+
+class SquareThread extends Thread {
+    public void run() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Square of " + i + ": " + (i * i));
+            try { Thread.sleep(500); } catch (InterruptedException e) {}
+        }
+    }
+}
+
+public class MultithreadingExample {
+    public static void main(String[] args) {
+        NumberThread t1 = new NumberThread();
+        SquareThread t2 = new SquareThread();
+
+        t1.start(); 
+        t2.start(); 
+}
